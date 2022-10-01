6726c5d4e8c61795c6229e12a068caf631a1d7b0
Dillon <43819022+Dcode99@users.noreply.github.com>
2022-10-01 01:16:36 -0400

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,11 +1,31 @@
 
 function up(){
 
-    docker-compose -f Network-Files/docker-compose.yaml up -d
-    
-    echo waiting 3m for start up process
-    sleep 3m
-    echo finishing of start up process  
+    service docker start
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker network prune
+    docker volume prune
+    docker image prune
+    docker network create iroha-network
+    docker run --name some-postgres \
+	    -e POSTGRES_USER=postgres \
+	    -e POSTGRES_PASSWORD=mysecretpassword \
+	    -p 5432:5432 \
+	    --network=iroha-network \
+	    -d postgres:9.5 \
+	    -c 'max_prepared_transactions=100'
+    docker volume create blockstore
+    # git clone -b main https://github.com/hyperledger/iroha --depth=1
+    docker run --name iroha \
+	    -d \
+	    -p 50051:50051 \
+	    -v $(pwd)/iroha/example:/opt/iroha_data \
+	    -v blockstore:/tmp/block_store \
+	    --network=iroha-network \
+	    -e KEY='node1' \
+	    hyperledger/iroha:latest
+
 }
 
 

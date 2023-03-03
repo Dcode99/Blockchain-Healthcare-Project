e18db929c37daccdce05b3ec2a5d022efde586ab
Dillon <43819022+Dcode99@users.noreply.github.com>
2023-03-03 10:46:55 -0500

    Creating a python setup file
    Unfinished, fix install requirements later

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pyhyperhealth',
+    version='0.1',
+    packages=['pyhyperhealth'],
+    url='https://github.com/Dcode99/Blockchain-Healthcare-Project/',
+    license='Apache 2.0',
+    author='Dillon Tate',
+    author_email='dillon.tate@uky.edu',
+    description='Python Hyperledger Iroha Healthcare Permissions Library',
+    install_requires=['websocket-client'],
+
+)

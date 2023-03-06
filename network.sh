0c9b08af5d329006cf2f2cca9b0b42884aa7ed58
Dillon <43819022+Dcode99@users.noreply.github.com>
2023-03-06 11:08:13 -0500

    Create network.sh

---
+++ network.sh
@@ -0,0 +1,74 @@
+
+function up_1(){
+
+    service docker start
+    docker run --name some-postgres \
+	    -e POSTGRES_USER=postgres \
+	    -e POSTGRES_PASSWORD=mysecretpassword \
+	    --net host \
+	    -d postgres:9.5 \
+	    -c 'max_prepared_transactions=100'
+    docker volume create blockstore
+    sleep 10s
+    docker run --name iroha \
+	    -d \
+	    --net host \
+	    -v $(pwd)/Network-Files/node1:/opt/iroha_data \
+	    -v blockstore:/tmp/block_store \
+	    -e KEY='node1' \
+	    hyperledger/iroha:latest
+
+}
+
+function up_2(){
+
+    service docker start
+    docker run --name some-postgres \
+	    -e POSTGRES_USER=postgres \
+	    -e POSTGRES_PASSWORD=mysecretpassword \
+	    --net host \
+	    -d postgres:9.5 \
+	    -c 'max_prepared_transactions=100'
+    docker volume create blockstore
+    sleep 10s
+    docker run --name iroha \
+	    -d \
+	    --net host \
+	    -v $(pwd)/Network-Files/node2:/opt/iroha_data \
+	    -v blockstore:/tmp/block_store \
+	    -e KEY='node2' \
+	    hyperledger/iroha:latest
+
+}
+
+function up_post(){
+
+    service docker start
+    docker run --name some-postgres \
+	    -e POSTGRES_USER=postgres \
+	    -e POSTGRES_PASSWORD=mysecretpassword \
+	    --net host \
+	    -d postgres:9.5 \
+	    -c 'max_prepared_transactions=100'
+}
+
+function down(){
+
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker network prune
+    docker volume prune
+    docker image prune
+}
+
+function restart_1(){
+    down
+    up_1
+}
+
+function restart_2(){
+    down
+    up_2
+}
+
+"$@"

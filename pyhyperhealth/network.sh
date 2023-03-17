209e230f51bbe92450eee41a6a5f0d4daec4f943
Dillon <43819022+Dcode99@users.noreply.github.com>
2023-03-17 12:34:51 -0400

    Update network.sh

--- pyhyperhealth/network.sh
+++ pyhyperhealth/network.sh
@@ -17,7 +17,6 @@ function up_1(){
 	    -v blockstore:/tmp/block_store \
 	    -e KEY='node1' \
 	    hyperledger/iroha:latest
-
 }
 
 function up_2(){
@@ -38,7 +37,27 @@ function up_2(){
 	    -v blockstore:/tmp/block_store \
 	    -e KEY='node2' \
 	    hyperledger/iroha:latest
+}
 
+function up_new(){
+
+    read -p "Enter folder name of keys: " keypair
+    service docker start
+    docker run --name some-postgres \
+	    -e POSTGRES_USER=postgres \
+	    -e POSTGRES_PASSWORD=mysecretpassword \
+	    --net host \
+	    -d postgres:9.5 \
+	    -c 'max_prepared_transactions=100'
+    docker volume create blockstore
+    sleep 10s
+    docker run --name iroha \
+	    -d \
+	    --net host \
+	    -v $(pwd)/$keypair:/opt/iroha_data \
+	    -v blockstore:/tmp/block_store \
+	    -e KEY='keypair' \
+	    hyperledger/iroha:latest
 }
 
 function up_post(){

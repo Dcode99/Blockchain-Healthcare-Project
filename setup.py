ba27101773ad3c26ae29723c242e748e19b7e14e
Dillon <43819022+Dcode99@users.noreply.github.com>
2023-03-18 00:21:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 setup(
     name='pyhyperhealth',
-    version='0.9',
+    version='1.0',
     packages=['pyhyperhealth'],
-    package_data={'pyhyperhealth': ['network.sh', 'Network_Files/*']},
+    package_data={'pyhyperhealth': ['network.sh', 'Network_Files/node1/config.docker', 'Network_Files/node2/config.docker', 'Network_Files/node3plus/config.docker', 
+                                    'Network_Files/node1/genesis.block', 'Network_Files/node2/genesis.block'], 'Network_Files/node3plus/genesis.block',
+                                    'Network_Files/node1/*.priv', 'Network_Files/node2/*.priv', 'Network_Files/node3plus/*.priv',
+                                    'Network_Files/node1/*.pub', 'Network_Files/node2/*.pub', 'Network_Files/node3plus/*.pub'},
     url='https://github.com/Dcode99/Blockchain-Healthcare-Project/',
     license='Apache 2.0',
     author='Dillon Tate',
